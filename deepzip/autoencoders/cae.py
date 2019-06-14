bc675663f7ec7e4e71a0507ac561f00ed68edf1d
Jake Grigsby <jcg6dn@virginia.edu>
2019-06-14 01:36:47 -0400

    adjust cae loss

--- deepzip/autoencoders/cae.py
+++ deepzip/autoencoders/cae.py
@@ -4,12 +4,13 @@ import deepzip as dz
 class ContractiveAutoEncoder(dz.core.BaseModel):
     def __init__(self, encoder_block, decoder_block, gamma):
         super().__init__(encode_block, decode_block)
+        self.gamma = gamma
 
+    @tf.function
     def compute_loss(self, x):
         h = self.encode(x)
-        dh_dx = tf.expand_dims(tf.convert_to_tensor(tf.gradients(h, x), dtype=tf.float32),0)
-        frob_norm = tf.norm(dh_dx, ord='fro')
         x_hat = self.decode(h)
-        loss = tf.losses.mean_squared_error(x, x_hat) + gamma * frob_norm
+        contractive_term = dz.loss.code_frobenius_norm(h, x)
+        loss = tf.losses.mean_squared_error(x, x_hat) + self.gamma * contractive_term
 
 
