aec9d9d3d75663059152147134556332ce6c9b81
Jake Grigsby <jcg6dn@virginia.edu>
2019-06-20 16:05:44 -0400

    coeffs for loss functions (using inner function pattern)

--- deepzip/loss.py
+++ deepzip/loss.py
@@ -9,79 +9,76 @@ import tensorflow as tf
 
 mse = tf.keras.losses.mean_squared_error
 
-"""
+def vae():
+    @tf.function
+    def _vae(model, x, original_x=None):
+        h = model.encode(x)
+        mean, logvar = tf.split(h, num_or_size_splits=2, axis=1)
+        z = reparameterize(mean, logvar)
+        x_logit = model.decode(z)
 
-class Reconstruction:
-    def __call__(self, model, x, original_x=None):
-        self.h = model.encode(x)
-        self.x_hat = model.decode(h)
-        return mse(x, self.x_hat)
+        cross_ent = tf.nn.sigmoid_cross_entropy_with_logits(logits=x_logit, labels=x)
+        logpx_z = -tf.reduce_sum(cross_ent, axis=[1, 2, 3])
+        logpz = log_normal_pdf(z, 0., 0.)
+        logqz_x = log_normal_pdf(z, mean, logvar)
+        return -tf.reduce_mean(logpx_z + logpz - logqz_x)
+    return _vae
 
-class Sparsity(Reconstruction):
-    def __init__(self, sparsity_coeff):
-        self.sparsity_coeff = sparsity_coeff
+def contractive(coeff):
+    @tf.function
+    def _contractive(model, x, original_x=None):
+        h = model.encode(x)
+        x_hat = model.decode(h)
+        dh_dx = tf.convert_to_tensor(tf.gradients(h, x), dtype=tf.float32)
+        frob_norm = tf.norm(dh_dx)
+        reconstruction = mse(x_hat, x)
+        return reconstruction + coeff*frob_norm
+    return _contractive
 
-    def __call__(self, model, x, original_x=None):
-        reconstruction = super().__call__(model, x, original_x)
-        sparsity = tf.norm(self.h, ord=1)
-        return reconstruction + self.sparsity_coeff*sparsity
-"""
+def denoising_contractive(coeff):
+    @tf.function
+    def _noisy_code_frobenius_norm(model, x, original_x=None):
+        h = model.encode(x)
+        x_hat = model.decode(h)
+        dh_dx = tf.convert_to_tensor(tf.gradients(h, x), dtype=tf.float32)
+        frob_norm = tf.norm(dh_dx)
+        noisy_reconstruction = mse(x_hat, original_x)
+        return noisy_reconstruction + coeff*frob_norm
+    return _noisy_code_frobenius_norm
 
-@tf.function
-def compute_loss_vae(model, x, original_x=None):
-    h = model.encode(x)
-    mean, logvar = tf.split(h, num_or_size_splits=2, axis=1)
-    z = reparameterize(mean, logvar)
-    x_logit = model.decode(z)
+def reconstruction_sparsity(coeff):
+    @tf.function
+    def _reconstruction_sparsity(model, x, original_x=None):
+        h = model.encode(x)
+        x_hat = model.decode(h)
+        reconstruction = mse(x, x_hat)
+        sparsity = tf.norm(h, ord=1)
+        return reconstruction + coeff*sparsity
+    return _reconstruction_sparsity
 
-    cross_ent = tf.nn.sigmoid_cross_entropy_with_logits(logits=x_logit, labels=x)
-    logpx_z = -tf.reduce_sum(cross_ent, axis=[1, 2, 3])
-    logpz = log_normal_pdf(z, 0., 0.)
-    logqz_x = log_normal_pdf(z, mean, logvar)
-    return -tf.reduce_mean(logpx_z + logpz - logqz_x)
+def denoising_sparsity(coeff):
+    @tf.function
+    def _denoising_sparsity(model, x, original_x=None):
+        h = model.encode(x)
+        x_hat = model.decode(h)
+        noisy_reconstruction = mse(x_hat, original_x)
+        sparsity = tf.norm(h, ord=1)
+        return noisy_reconstruction + coeff*sparsity
+    return _denoising_sparsity
 
-@tf.function
-def code_frobenius_norm(model, x, original_x=None, coeff=.1):
-    h = model.encode(x)
-    x_hat = model.decode(h)
-    dh_dx = tf.convert_to_tensor(tf.gradients(h, x), dtype=tf.float32)
-    frob_norm = tf.norm(dh_dx)
-    reconstruction = mse(x_hat, x)
-    return reconstruction + coeff*frob_norm
+def denoising():
+    @tf.function
+    def _denoising(model, x, original_x=None):
+        h = model.encode(x)
+        x_hat = model.decode(h)
+        return mse(x_hat, original_x)
+    return _denoising
 
-@tf.function
-def noisy_code_frobenius_norm(model, x, original_x=None, coeff=.1):
-    h = model.encode(x)
-    x_hat = model.decode(h)
-    dh_dx = tf.convert_to_tensor(tf.gradients(h, x), dtype=tf.float32)
-    frob_norm = tf.norm(dh_dx)
-    noisy_reconstruction = mse(x_hat, original_x)
-    return noisy_reconstruction + coeff*frob_norm
-
-@tf.function
-def reconstruction_sparsity(model, x, original_x=None, coeff=.1):
-    h = model.encode(x)
-    x_hat = model.decode(h)
-    reconstruction = mse(x, x_hat)
-    sparsity = tf.norm(h, ord=1)
-    return reconstruction + coeff*sparsity
-
-@tf.function
-def noisy_reconstruction_sparsity(model, x, original_x=None, coeff=.1):
-    h = model.encode(x)
-    x_hat = model.decode(h)
-    noisy_reconstruction = mse(x_hat, original_x)
-    sparsity = tf.norm(h, ord=1)
-    return noisy_reconstruction + coeff*sparsity
-
-@tf.function
-def noisy_reconstruction(model, x, original_x=None):
-    h = model.encode(x)
-    x_hat = model.decode(h)
-    return mse(x_hat, original_x)
-
-@tf.function
-def reconstruction(model, x, original_x=None):
-    h = model.encode(x)
-    x_hat = model.decode(h)
-    return mse(x, x_hat)
+def reconstruction():
+    @tf.function
+    def _reconstruction(model, x, original_x=None):
+        h = model.encode(x)
+        x_hat = model.decode(h)
+        return mse(x, x_hat)
+    return _reconstruction
+    
--- deepzip/recipes.py
+++ deepzip/recipes.py
@@ -5,13 +5,13 @@ from . import loss
 #@TODO make this pattern into a decorator
 
 def ContractiveAutoEncoder(encoder, decoder):
-    return AutoEncoder(encoder, decoder, loss=loss.code_frobenius_norm)
+    return AutoEncoder(encoder, decoder, loss=loss.contractive(.1))
 
 def DenoisingAutoEncoder(encoder, decoder, gamma):
-    return AutoEncoder(encoder, decoder, preprocessing_steps=[preprocessing.random_mask(gamma)])
+    return AutoEncoder(encoder, decoder, preprocessing_steps=[preprocessing.random_mask(gamma)], loss=loss.reconstruction_sparsity(.1))
 
 def VariationalAutoEncoder(encoder, decoder):
-    return AutoEncoder(encoder, decoder, loss=loss.compute_loss_vae)
+    return AutoEncoder(encoder, decoder, loss=loss.vae())
 
 def SparseAutoEncoder(encoder, decoder):
-    return AutoEncoder(encoder, decoder, loss=loss.reconstruction_sparsity)
+    return AutoEncoder(encoder, decoder, loss=loss.reconstruction_sparsity(.1))
