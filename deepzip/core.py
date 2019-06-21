4aafb522619f52405ade6f431980d0a1a439d8a3
Jake Grigsby <jcg6dn@virginia.edu>
2019-06-21 16:32:53 -0400

    new loss api. doesn't work yet

--- deepzip/core.py
+++ deepzip/core.py
@@ -6,31 +6,54 @@ import numpy as np
 import tensorflow as tf
 
 from .loss import reconstruction
+from . import helpers
 
 class AutoEncoder(tf.keras.Model):
     """ A basic autoencoder.
     """
-    def __init__(self, encode_block, decode_block, preprocessing_steps=[], loss=reconstruction()):
+    def __init__(self, encode_block, decode_block, preprocessing_steps=[], call_func='ae', loss_funcs=[reconstruction()]):
         super().__init__()
         self.encoder, self.decoder = encode_block(), decode_block()
         self.preprocessing_steps = preprocessing_steps
-        self.compute_loss = functools.partial(loss, self)
+        self.loss_funcs = loss_funcs
+        self.choose_call_func(call_func)
+
+    def choose_call_func(self, call_func):
+        if call_func == 'ae':
+            self.call = self.call_ae
+        elif call_func == 'vae':
+            self.call = self.call_vae
+        else:
+            raise ValueError(f"Unrecognized value for call_func: {call_func}. Options are 'ae' and 'vae'.")
 
     def preprocess_input(self, x):
         for func in self.preprocessing_steps:
             x = func(x)
         return x
     
+    def compute_loss(self, *args):
+        loss = 0
+        for loss_func in self.loss_funcs:
+            loss += loss_func(*args)
+        return loss
+    
     @property
     def trainable_variables(self):
         return self.encoder.trainable_variables + self.decoder.trainable_variables
-
-    def call(self, x):
+    
+    def call_ae(self, original_x, x):
         """ Approximates x by encoding and decoding it.
         """
         h = self.encode(x)
         x_hat = self.decode(h)
-        return x_hat
+        return original_x, x, h, x_hat
+    
+    def call_vae(self, original_x, x):
+        h = self.encode(x)
+        mean, logvar = tf.split(h, num_or_size_splits=2, axis=1)
+        z = helpers.reparameterize(mean, logvar)
+        x_logit = self.decode(z)
+        return original_x, x, h, mean, logvar, z, x_logit
 
     def encode(self, x):
         return self.encoder(x)
@@ -47,21 +70,19 @@ class AutoEncoder(tf.keras.Model):
         while os.path.exists(directory + f"_{i}"):
             i += 1
         directory += f"_{i}"
-
         # Setup checkpoints and logging
         checkpoint_dir = os.path.join(directory, 'checkpoints')
         os.makedirs(checkpoint_dir)
-
         log_dir = os.path.join(directory, 'logs')
         os.makedirs(log_dir)
-
         return log_dir, checkpoint_dir
 
     def compute_gradients(self, x, original_x):
         """ Computes gradient of custom loss function.
         """
         with tf.GradientTape() as tape:
-          loss = self.compute_loss(x, original_x)
+            call_result = self.call(original_x, x)
+            loss = self.compute_loss(*call_result)
         grad = tape.gradient(loss, self.trainable_variables)
         return grad, loss
 
@@ -98,7 +119,7 @@ class AutoEncoder(tf.keras.Model):
             val_loss = tf.keras.metrics.Mean()
             for original_x in val_dataset:
                 processed_x = self.preprocess_input(original_x)
-                val_loss.update_state(self.compute_loss(processed_x, original_x))
+                val_loss.update_state(self.compute_loss(self.call(original_x, processed_x)))
             val_loss = val_loss.result().numpy()
             if verbosity == 1:
                 print('Epoch: {}, Test set total loss: {}, '
--- deepzip/recipes.py
+++ deepzip/recipes.py
@@ -5,13 +5,13 @@ from . import loss
 #@TODO make this pattern into a decorator
 
 def ContractiveAutoEncoder(encoder, decoder, gamma):
-    return AutoEncoder(encoder, decoder, loss=loss.contractive(gamma))
+    return AutoEncoder(encoder, decoder, loss_funcs=[loss.reconstruction(), loss.contractive(.1)])
 
 def DenoisingAutoEncoder(encoder, decoder, gamma):
-    return AutoEncoder(encoder, decoder, preprocessing_steps=[preprocessing.random_mask(gamma)], loss=loss.denoising())
+    return AutoEncoder(encoder, decoder, preprocessing_steps=[preprocessing.random_mask(gamma)], loss_funcs=[loss.denoising()])
 
 def VariationalAutoEncoder(encoder, decoder):
-    return AutoEncoder(encoder, decoder, loss=loss.vae())
+    return AutoEncoder(encoder, decoder, call_func='vae', loss_funcs=[loss.vae()])
 
 def SparseAutoEncoder(encoder, decoder, gamma):
-    return AutoEncoder(encoder, decoder, loss=loss.reconstruction_sparsity(gamma))
+    return AutoEncoder(encoder, decoder, loss_funcs=[loss.reconstruction(), loss.sparsity(gamma)])
