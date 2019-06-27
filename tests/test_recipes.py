5aaa478d0fcdb81652f12c9ee19371ca8ab367bf
Jack Morris <jxmorris12@gmail.com>
2019-06-26 20:48:41 -0400

    maybe the vae just doesnt work..?

--- tests/test_recipes.py
+++ tests/test_recipes.py
@@ -8,25 +8,22 @@ from deepzip.nets.decoders import EasyDecoder
 
 x_train, x_val = dz.data.cifar10.load(64)
 
-encoder = EasyEncoder()
-decoder = EasyDecoder()
-
 def test_default():
-    model = dz.core.AutoEncoder(encoder, decoder)
+    model = dz.core.AutoEncoder(EasyEncoder(), EasyDecoder())
     model.train(x_train, x_val, epochs=1, experiment_name='test_default', verbosity=0)
 
 def test_vae():
-    model = dz.recipes.VariationalAutoEncoder(encoder, decoder)
+    model = dz.recipes.VariationalAutoEncoder(EasyEncoder(), EasyDecoder())
     model.train(x_train, x_val, epochs=1, experiment_name='test_default', verbosity=0)
 
 def test_denoising():
-    model = dz.recipes.DenoisingAutoEncoder(encoder, decoder, .1)
+    model = dz.recipes.DenoisingAutoEncoder(EasyEncoder(), EasyDecoder(), .1)
     model.train(x_train, x_val, epochs=1, experiment_name='test_default', verbosity=0)
 
 def test_sparse():
-    model = dz.recipes.SparseAutoEncoder(encoder,decoder, .1)
+    model = dz.recipes.SparseAutoEncoder(EasyEncoder(),EasyDecoder(), .1)
     model.train(x_train, x_val, epochs=1, experiment_name='test_default', verbosity=0)
 
 def test_contractive():
-    model = dz.recipes.ContractiveAutoEncoder(encoder, decoder, .1)
+    model = dz.recipes.ContractiveAutoEncoder(EasyEncoder(), EasyDecoder(), .1)
     model.train(x_train, x_val, epochs=1, experiment_name='test_default', verbosity=0)
