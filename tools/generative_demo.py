6dc8054eb10f6c015269b86ac07cd99ff6e40121
Jack Morris <jxmorris12@gmail.com>
2019-06-29 21:19:43 -0400

    folder for tracking gifs

--- tools/generative_demo.py
+++ tools/generative_demo.py
@@ -1,4 +1,6 @@
 import math
+import os
+import time
 
 import deepzip as dz
 import tensorflow as tf
@@ -13,9 +15,16 @@ show_plots = False
 x_train, x_val = dz.data.cifar10.load()
 latent_dim = 16
 
+folder_name = 'vae-test-{}'.format(int(time.time()))
+os.mkdir(folder_name)
+print('Created directory {}.'.format(folder_name))
+num_epochs = 10
 vae = dz.recipes.VariationalAutoEncoder(EasyEncoder(), EasyDecoder(), latent_dim=latent_dim)
 
 num_examples_to_generate = 16
+num_rows = math.floor(math.sqrt(num_examples_to_generate))
+num_cols = math.ceil(num_examples_to_generate / num_rows)
+
 random_vector_for_generation = tf.random.normal(shape=[num_examples_to_generate, latent_dim])
 
 print('random_vector_for_generation:', random_vector_for_generation)
@@ -27,17 +36,14 @@ def generate_and_save_images(model):
   predictions = model.sample(random_vector_for_generation)
   # print('predictions:', predictions)
   fig = plt.figure(figsize=(4,4))
-  
-  num_rows = math.floor(math.sqrt(num_examples_to_generate))
-  num_cols = math.ceil(num_examples_to_generate / num_rows)
 
   for i in range(predictions.shape[0]):
       plt.subplot(num_rows, num_cols, i+1)
-      plt.imshow(predictions[i, :, :, 0], cmap='gray')
+      plt.imshow(predictions[i, :, :, 0])
       plt.axis('off')
 
   # tight_layout minimizes the overlap between 2 sub-plots
-  plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
+  plt.savefig('{}/image_at_epoch_{:04d}.png'.format(folder_name, epoch))
   
   if show_plots:
     plt.show()
@@ -47,5 +53,5 @@ _i = 0
 def callback(): 
   generate_and_save_images(vae, random_vector_for_generation)
 
-vae.train(x_train, x_val, epochs=10, experiment_name='test_default', verbosity=1, callbacks=[generate_and_save_images])
+vae.train(x_train, x_val, epochs=num_epochs, experiment_name='test_default', verbosity=1, callbacks=[generate_and_save_images])
     
