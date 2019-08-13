a4800fe9f6cbf7bc216b253d52afb0e5323524f8
Jake Grigsby <jcg6dn@virginia.edu>
2019-08-13 00:34:07 -0400

    maximum mean discrepancy

--- daze/loss.py
+++ daze/loss.py
@@ -43,15 +43,13 @@ def elbo():
     """
     @trace_graph
     def _elbo(**forward_pass):
-        x_hat = forward_pass["x_hat"]
-        x = forward_pass["x"]
-        z = forward_pass["z"]
-        cross_ent = tf.nn.sigmoid_cross_entropy_with_logits(logits=x_hat, labels=x)
-        logpx_z = -tf.reduce_sum(cross_ent, axis=[1, 2, 3])
-        logpz = log_normal_pdf(z, 0.0, 0.0)
-        logqz_x = log_normal_pdf(z, mean, logvar)
-        return -tf.reduce_mean(logpx_z + logpz - logqz_x)
-
+        mean = forward_pass["mean"]
+        stddev = forward_pass["stddev"]
+        return tf.math.reduce_mean(
+            tf.math.reduce_sum(
+                -tf.math.log(stddev) + .5*tf.math.square(stddev) + .5*tf.math.square(mean) - .5, axis=1
+            )
+        )
     return _elbo
 
 
@@ -140,3 +138,26 @@ def sparsity(rho, beta):
         return beta * tf.keras.losses.kld(rho, rho_hat)
 
     return _sparsity
+
+def maximum_mean_discrepancy():
+    """Maximum Mean Discrepancy
+
+    Paper: https://arxiv.org/pdf/1706.02262.pdf
+    """
+    def compute_kernel(x, y):
+        x_size = tf.shape(x)[0]
+        y_size = tf.shape(y)[0]
+        dim = tf.shape(x)[1]
+        tiled_x = tf.tile(tf.reshape(x, tf.stack([x_size, 1, dim])), tf.stack([1, y_size, 1]))
+        tiled_y = tf.tile(tf.reshape(y, tf.stack([1, y_size, dim])), tf.stack([x_size, 1, 1]))
+        return tf.exp(-tf.reduce_mean(tf.square(tiled_x - tiled_y), axis=2) / tf.cast(dim, tf.float32))
+
+    def _maximum_mean_discrepancy(**forward_pass):
+        x = forward_pass["x"]
+        z = forward_pass["z"]
+        x_kernel = compute_kernel(x, x)
+        z_kernel = compute_kernel(z, z)
+        xz_kernel = compute_kernel(x, z)
+        return tf.reduce_mean(x_kernel) + tf.reduce_mean(z_kernel) - 2 * tf.reduce_mean(xz_kernel)
+
+       
