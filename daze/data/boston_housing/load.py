3ad41638817d4f0b8437ee1fd1237322a9f399be
Jake Grigsby <jcg6dn@virginia.edu>
2019-08-16 17:04:51 -0400

    data module cleanups/tests
    Former-commit-id: 1965797c0bb78704cbdefd323b513d29865a49d4

--- daze/data/boston_housing/load.py
+++ daze/data/boston_housing/load.py
@@ -4,11 +4,14 @@ import numpy as np
 import daze as dz
 
 
-def load(size=None, dtype=None):
-    (x_train, _), (x_val, _) = tf.keras.datasets.boston_housing.load_data()
+def load(size=None, dtype=None, return_labels=False):
+    (x_train, y_train), (x_val, y_val) = tf.keras.datasets.boston_housing.load_data()
     size = x_train.shape[0] if not size else size
-    x_train, x_val = x_train[:size], x_val[:size]
+    x_train, y_train, x_val, y_val = x_train[:size], y_train[:size], x_val[:size], y_val[:size]
     if dtype:
         dtype = dz.data.utils.parse_dtype(dtype)
         x_train, x_val = x_train.astype(dtype), x_val.astype(dtype)
-    return x_train, x_val
+    if return_labels:
+        return (x_train, y_train), (x_val, y_val)
+    else:
+        return x_train, x_val
--- tests/test_data.py
+++ tests/test_data.py
@@ -6,6 +6,121 @@ import daze as dz
 from daze.nets.encoders import EasyEncoder
 from daze.nets.decoders import EasyDecoder
 
+###########
+## MNIST ##
+###########
+
+def test_mnist_batch():
+    x_train, x_val = dz.data.mnist.load(64)
+    assert x_train.shape[0] == 64
+    assert x_val.shape[0] == 64
+    assert x_train.shape[1] == 28
+    assert x_train.shape[1] == x_train.shape[2]
+
+def test_mnist_all():
+    x_train, x_val = dz.data.mnist.load()
+    assert x_train.shape[0] > x_val.shape[0]
+
+def test_mnist_return_labels():
+    (x_train, y_train), (x_val, y_val) = dz.data.mnist.load(return_labels=True)
+    assert x_train.shape[0] == y_train.shape[0]
+    assert x_val.shape[0] == y_val.shape[0]
+
+def test_mnist_float():
+    x_train, x_val = dz.data.mnist.load(64, dtype=np.float32)
+    assert x_train.dtype == np.float32
+    x_train, x_val = dz.data.mnist.load(64, dtype="f32")
+    assert x_train.dtype == np.float32
+
+#############
+## CIFAR10 ##
+#############
+
+def test_cifar10_batch():
+    x_train, x_val = dz.data.cifar10.load(64)
+    assert x_train.shape[0] == 64
+    assert x_val.shape[0] == 64
+    assert x_train.shape[1] == 32
+    assert x_train.shape[1] == x_train.shape[2]
+
+
+def test_cifar10_all():
+    x_train, x_val = dz.data.cifar10.load()
+    assert x_train.shape[0] > x_val.shape[0]
+
+def test_cifar10_return_labels():
+    (x_train, y_train), (x_val, y_val) = dz.data.cifar10.load(return_labels=True)
+    assert x_train.shape[0] == y_train.shape[0]
+    assert x_val.shape[0] == y_val.shape[0]
+
+def test_cifar10_float():
+    x_train, x_val = dz.data.cifar10.load(dtype="f32")
+    assert x_train.dtype == np.float32
+    x_train, x_val = dz.data.cifar10.load(dtype=np.float32)
+    assert x_train.dtype == np.float32
+
+####################
+## BOSTON HOUSING ##
+####################
+
+def test_boston_batch():
+    x_train, x_val = dz.data.boston_housing.load(64)
+    assert x_train.shape[0] == 64
+    assert x_val.shape[0] == 64
+    assert x_train.shape[1] == 13
+
+def test_boston_float32():
+    x_train, x_val = dz.data.boston_housing.load(dtype="f32")
+    assert x_train.dtype == np.float32
+    x_train, x_val = dz.data.boston_housing.load(dtype=np.float32)
+    assert x_train.dtype == np.float32
+
+def test_boston_all():
+    x_train, x_val = dz.data.boston_housing.load(dtype="f32")
+    assert x_train.shape[0] > x_val.shape[0]
+
+def test_boston_return_labels():
+    (x_train, y_train), (x_val, y_val) = dz.data.boston_housing.load(return_labels=True)
+    assert x_train.shape[0] == y_train.shape[0]
+    assert x_val.shape[0] == y_val.shape[0]
+
+
+###################
+## FASHION MNIST ##
+###################
+
+def test_fashionmnist_all():
+    x_train, x_val = dz.data.fashionmnist.load(dtype="f32")
+    assert x_train.shape[1:] == (28, 28, 1)
+    assert x_train.shape[0] > x_val.shape[0]
+
+def test_fashionmnist_batch():
+    x_train, x_val = dz.data.fashionmnist.load(64)
+    assert x_train.shape[0] == 64
+    assert x_val.shape[0] == 64
+
+def test_fashionmnist_float():
+    x_train, x_val = dz.data.fashionmnist.load(dtype=np.float32)
+    assert x_train.dtype == np.float32
+    x_train, x_val = dz.data.fashionmnist.load(dtype="f32")
+    assert x_train.dtype == np.float32
+
+def test_fashionmnist_return_labels():
+    (x_train, y_train), (x_val, y_val) = dz.data.fashionmnist.load(return_labels=True)
+    assert x_train.shape[0] == y_train.shape[0]
+    assert x_val.shape[0] == y_val.shape[0]
+
+##########
+## MISC ##
+##########
+
+def test_np_to_tf():
+    x_train, _ = dz.data.cifar10.load(64)
+    x_train, batch_count = dz.data.utils.convert_np_to_tf(
+        x_train, batch_size=32, return_batch_count=True
+    )
+    assert isinstance(x_train, tf.data.Dataset)
+    assert batch_count == 2
 
 def test_default_dtypes():
     """
@@ -17,6 +132,8 @@ def test_default_dtypes():
     assert cifar.dtype == np.uint8
     boston, _ = dz.data.boston_housing.load(1)
     assert boston.dtype == np.float64
+    fashion, _ = dz.data.fashionmnist.load(1)
+    assert fashion.dtype == np.int64
 
 
 def test_parse_dtype():
@@ -63,60 +180,3 @@ def test_get_byte_count():
     assert f("f8") == 8
     assert f("f16") == 16
     assert f("fljlhlh32") == 32
-
-
-def test_mnist_batch():
-    x_train, x_val = dz.data.mnist.load(64)
-    assert x_train.shape[0] == 64
-    assert x_val.shape[0] == 64
-    assert x_train.shape[1] == 28
-    assert x_train.shape[1] == x_train.shape[2]
-
-
-def test_mnist_all():
-    x_train, x_val = dz.data.mnist.load()
-    assert x_train.shape[0] > x_val.shape[0]
-
-
-def test_mnist_float():
-    x_train, x_val = dz.data.mnist.load(64, dtype=np.float32)
-
-
-def test_cifar10_batch():
-    x_train, x_val = dz.data.cifar10.load(64)
-    assert x_train.shape[0] == 64
-    assert x_val.shape[0] == 64
-    assert x_train.shape[1] == 32
-    assert x_train.shape[1] == x_train.shape[2]
-
-
-def test_cifar10_all():
-    x_train, x_val = dz.data.cifar10.load()
-    assert x_train.shape[0] > x_val.shape[0]
-
-
-def test_boston_batch():
-    x_train, x_val = dz.data.boston_housing.load(64)
-    assert x_train.shape[0] == 64
-    assert x_val.shape[0] == 64
-    assert x_train.shape[1] == 13
-
-
-def test_boston_all():
-    x_train, x_val = dz.data.boston_housing.load(dtype="f32")
-    assert x_train.shape[0] > x_val.shape[0]
-
-
-def test_fashionmnist():
-    x_train, x_val = dz.data.fashionmnist.load(dtype="f32")
-    assert x_train.shape[1:] == (28, 28, 1)
-    assert x_train.shape[0] > x_val.shape[0]
-
-
-def test_np_to_tf():
-    x_train, _ = dz.data.cifar10.load(64)
-    x_train, batch_count = dz.data.utils.convert_np_to_tf(
-        x_train, batch_size=32, return_batch_count=True
-    )
-    assert isinstance(x_train, tf.data.Dataset)
-    assert batch_count == 2
