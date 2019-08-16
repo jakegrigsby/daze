c4b25b1c8820c1ce2c3b15af3157e65d519265a7
Jake Grigsby <jcg6dn@virginia.edu>
2019-08-16 16:08:52 -0400

    3dchairs utils

---
+++ daze/data/chairs/load.py
@@ -0,0 +1,9 @@
+
+import numpy as np
+
+import daze as dz
+from daze.data.utils import relative_path, download, untar_if_tarred
+
+
+def load(size=None, dtype='f32'):
+    raise NotImplementedError()
