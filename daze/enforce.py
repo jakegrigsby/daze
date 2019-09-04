2ac50a2266d8d3cb158a2625b60ddc3c4b29339d
Jake Grigsby <jcg6dn@virginia.edu>
2019-09-04 01:06:15 -0400

    remove string type indicators. just use OO hierarchy and isinstance
    Former-commit-id: 78f29ca3ed61889cc77528bd39386b20f6da9a34

--- daze/enforce.py
+++ daze/enforce.py
@@ -6,40 +6,26 @@ class compatible:
     def __init__(self, func):
         self.func = func
         functools.update_wrapper(self, func)
-        self.func._compatability = []
-
-    def add_compatability(self, code):
-        self.func._compatability.append(code)
 
     def __call__(self, *args, **kwargs):
         return self.func(*args, **kwargs)
-    
-    @property
-    def compatability(self):
-        return self.func._compatability
 
 class ae_compatible(compatible):
     def __init__(self, func):
         super().__init__(func)
-        self.add_compatability("AE")
 
 class vae_compatible(ae_compatible):
     def __init__(self, func):
         super().__init__(func)
-        self.add_compatability("VAE")
 
 class gan_compatible(compatible):
     def __init__(self, func):
         super().__init__(func)
-        self.add_compatability("GAN")
 
 class discriminator_compatible(gan_compatible):
     def __init__(self, func):
         super().__init__(func)
-        self.add_compatability("DISC")
 
 class generator_compatible(gan_compatible):
     def __init__(self, func):
         super().__init__(func)
-        self.add_compatability("GEN")
-
